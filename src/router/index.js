93e23e733daf14396cd3834488406a6a4e1ccf4a
THXWXT <tanawatflook@hotmail.com>
2024-07-21 06:41:46 +0700

    clear all file not use

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import UserList from '../components/UserList.vue'
 import UserUpdate from '@/views/UserUpdate.vue'
 import UserAdd from '@/views/UserAdd.vue'
